82ac87ccaee8623e50cfa5d19af0d697e5b85d47
Matthew <matthewdoncates@gmail.com>
2017-05-13 13:19:49 -0500

    Added basic structure

---
+++ assets/php/header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Yggdrasil</title>
+</head>
+<body>
+	
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php require_once ("assets/php/header.php"); ?>
+
+ Test
+
+<?php require_once ("assets/php/footer.php") ?>
